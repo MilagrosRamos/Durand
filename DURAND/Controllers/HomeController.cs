4dd3088ee81682ff864be474cc84ef4deb095f89
Luna Houli <lunahouli@hotmail.com>
2019-10-11 10:30:33 -0300

    cambios en diseño y medicos service

--- DURAND/Controllers/HomeController.cs
+++ DURAND/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace DURAND.Controllers
 {
     public class HomeController : Controller
     {
+
+        //ACCIONES
         public ActionResult Login()
         {
             return View();
@@ -21,6 +23,7 @@ namespace DURAND.Controllers
         {
             Medico MedicoTest;
             MedicoTest = MedicosService.ObtenerPorMailyContrasenia(unMedico.Mail, unMedico.Contrasenia);
+            //System.Web.HttpContext.Current.Session["sessionMedico"] = MedicoTest;
             if (MedicoTest != null)
             {
                 return View("PantallaPrincipal");
@@ -65,6 +68,13 @@ namespace DURAND.Controllers
             int RegsAfec            = MedicosService.AgregarMedico(unMedico);
 
             return View("Login");
+        }
+
+        public ActionResult ModificarMedico()
+        {
+            IEnumerable<SelectListItem> especialidades = EspecialidadesService.ObtenerTodosDropDown().ToList();
+            ViewBag.especialidadList = especialidades;
+            return View();
         }
 
         //PACIENTE
@@ -197,6 +207,15 @@ namespace DURAND.Controllers
 
             return View("HistoriaClinica", elPaciente);
         }
+
+        //LAYOUT
+
+        //public ActionResult LayoutInterno(Medico unMedico)
+        //{
+        //    Medico MedicoNombreYApellido = MedicosService.ObtenerPorMail(unMedico.Mail);
+        //    ViewBag.NombreYApellidoMedico = MedicoNombreYApellido.Apellido + ", " + MedicoNombreYApellido.Nombre;
+        //    return View();
+        //}
 
     }
 }
